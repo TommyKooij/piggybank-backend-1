e264bea53193143a46f3b4999ba0bd2e18d0eadd
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-01-31 10:59:13 +0100

    31/01/2024
    - Added more Unit Tests

--- src/test/java/com/testing/piggybank/TransactionServiceTest_UnitTests.java
+++ src/test/java/com/testing/piggybank/TransactionServiceTest_UnitTests.java
@@ -5,6 +5,9 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 import org.h2.tools.Console;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -28,13 +32,13 @@ import com.testing.piggybank.transaction.TransactionService;
 @ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest_UnitTests {
 	@Mock
-	private TransactionService transactionService;
-	@Mock
 	private TransactionRepository transactionRepository;
 	@Mock
 	private AccountService accountService;
 	@Mock
 	private CurrencyConverterService converterService;
+	@InjectMocks
+	private TransactionService transactionService;
 	
 	@Test
 	public void filterAndLimitTest_emptyListCheck() {
@@ -42,25 +46,49 @@ public class TransactionServiceTest_UnitTests {
 		List<Transaction> transactionList = Collections.emptyList();
         long accountId = 1L;
         int limit = 2;
+        
         //Act
         List<Transaction> result = transactionService.filterAndLimitTransactions(transactionList, accountId, limit);
+        
         //Assert
         Assertions.assertEquals(0, result.size());
 	}
 	
 	@Test
-	public void createTransaction_transactionSuccessful() {
+	public void createTransaction_calledSaveFunction() {
 		//List
+		Mockito.when(accountService.getAccount(1L)).thenReturn(Optional.of(new Account()));
+		Mockito.when(accountService.getAccount(2L)).thenReturn(Optional.of(new Account()));
+		Mockito.when(converterService.toEuro(Currency.EURO, new BigDecimal(200))).thenReturn(new BigDecimal(1));
+		
 		CreateTransactionRequest transactionRequest = new CreateTransactionRequest();
 		transactionRequest.setSenderAccountId(1L);
 		transactionRequest.setReceiverAccountId(2L); 
-		transactionRequest.setAmount(new BigDecimal("1.00")); 
+		transactionRequest.setAmount(new BigDecimal(200)); 
 		transactionRequest.setCurrency(Currency.EURO);
 		transactionRequest.setDescription("CreateTransaction Test"); 
+		
 		//Act
 		transactionService.createTransaction(transactionRequest); 
-		List<Transaction> transactionResult = transactionRepository.findAllByReceiverAccount_Id(2L);
+		
 		//Assert
-		Assertions.assertTrue(transactionResult.size() == 0); 
+		Mockito.verify(transactionRepository).save(Mockito.any());
+	}
+	
+	@Test
+	public void sortsDescByDateTime_returnedValue() {
+		//List
+		Transaction transaction1 = new Transaction();
+		Transaction transaction2 = new Transaction();
+		transaction1.setDateTime(Instant.now());
+		transaction2.setDateTime(ZonedDateTime.now().minusYears(5).toInstant());
+		
+		//Act
+		int result = transactionService.sortDescByDateTime(transaction1, transaction2);
+		
+		//Assert
+		Assertions.assertNotNull(result);
+		//Gives -1 if second time came before first, and 1 if second time came after first
+		Assertions.assertTrue(result == -1);
 	}
 }
